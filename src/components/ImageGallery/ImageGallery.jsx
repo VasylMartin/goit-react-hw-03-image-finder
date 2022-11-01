e819787645d6e7f8a05724f91ab67366282b4af5
VasylMartin <vasiliymartiniouk@gmail.com>
2022-11-01 19:00:37 +0200

    RHW3 T2 button with pages done

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -3,12 +3,12 @@ import React from "react";
 
 class ImageGallery extends React.Component {
 
-    state ={
+    state = {
         query: [],
     }
 
     componentDidUpdate(prevProps, prevState) {
-        if(prevProps.input !== this.props.input || prevProps.page !== this.state.page) {
+        if(prevProps.input !== this.props.input || prevProps.page !== this.props.page) {
             fetch(`https://pixabay.com/api/?q=${this.props.input}&page=${this.props.page}&key=29632801-66d18c979cc1b04cff9f90142&image_type=photo&orientation=horizontal&per_page=12`)
             .then(response => response.json())
             .then(query => {
@@ -26,13 +26,6 @@ class ImageGallery extends React.Component {
             })
             }
     }
-
-    // if(prevProps.query === this.props.query) {
-    //     this.setState(prevState =>({
-    //         query: [...prevState.query, ...query.hits],
-    //     }
-    //     ))
-    // }
 
     render() {
         return(
